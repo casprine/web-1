e9d56ba1a58ef80483a243a5d6d7e0d54a361687
Casprine Assempah <casprine.001@gmail.com>
2018-07-24 01:33:15 +0000

    right side of home content

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -2,16 +2,39 @@
     <div class="content">
         <div class="content-data">
             <div class="left">
-                Hi <span> 👋, </span> I am Casprine. <br>
-                A <span class="tag">frontend</span> developer and UI designer @ <a href="https://omgdigitalinc.com/" class="link"> OmgDigital.</a> <br>
-                        I design and develop experiences that make people's lives simple.
+                Hi <span> 👋, </span> I am Casprine. <br> A <span class="tag">frontend</span> developer and UI designer @ <a href="https://omgdigitalinc.com/" class="link"> OmgDigital.</a> <br> I design and develop experiences that make people's lives simple.
+                <br> I also write about my experiences and tutorials on my <a href="https://blog.casprine.xyz" class="link">Blog</a> <br>
             </div>
-
-
             <div class="right">
-                     Hi <span> 👋, </span> I am Casprine. A <span class="tag">
-                        frontend</span> developer @ <a href="https://omgdigitalinc.com/" class="link"> OmgDigital.</a> <br>
-                        I design and develop experiences that make people's lives simple.
+                <div class="stacks">
+                    <h2>Stack 👨‍💻 </h2>
+                    <div class="stack">
+                        <span class="stack-key"> Skills:</span> <span class="stack-data"> HTML, CSS(Sass),  Javascript, Git </span> <br>
+                        <span class="stack-key"> Frameworks:</span> <span class="stack-data"> React(Gatsby) , Vue </span> <br>
+                        <span class="stack-key"> Build Tools:</span> <span class="stack-data"> Gulp, parcel.js, webpack </span> <br>
+                        <span class="stack-key"> UI Tools:</span> <span class="stack-data"> Uxpin , Figma, Adobe XD </span> <br>
+                    </div>
+                </div>
+                <div class="links">
+                    <ul>
+                        <li>
+                            <a href="https://casprine.xyz">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M460.6 147.3L353 256.9c-.8.8-.8 2 0 2.8l75.3 80.2c5.1 5.1 5.1 13.3 0 18.4-2.5 2.5-5.9 3.8-9.2 3.8s-6.7-1.3-9.2-3.8l-75-79.9c-.8-.8-2.1-.8-2.9 0L313.7 297c-15.3 15.5-35.6 24.1-57.4 24.2-22.1.1-43.1-9.2-58.6-24.9l-17.6-17.9c-.8-.8-2.1-.8-2.9 0l-75 79.9c-2.5 2.5-5.9 3.8-9.2 3.8s-6.7-1.3-9.2-3.8c-5.1-5.1-5.1-13.3 0-18.4l75.3-80.2c.7-.8.7-2 0-2.8L51.4 147.3c-1.3-1.3-3.4-.4-3.4 1.4V368c0 17.6 14.4 32 32 32h352c17.6 0 32-14.4 32-32V148.7c0-1.8-2.2-2.6-3.4-1.4z"/><path d="M256 295.1c14.8 0 28.7-5.8 39.1-16.4L452 119c-5.5-4.4-12.3-7-19.8-7H79.9c-7.5 0-14.4 2.6-19.8 7L217 278.7c10.3 10.5 24.2 16.4 39 16.4z"/></svg>                            </a>
+                        </li>
+                        <li>
+                            <a href="https://dribbble.com/casprine">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="dribbble"><path d="M256 64C150 64 64 150 64 256c0 106.1 86 192 192 192s192-85.9 192-192c0-106-86-192-192-192zm121.9 88.5c21.6 25.4 35.3 57.6 37.7 92.9-34.6-1.8-76-1.8-109.2 1.3-4.2-10.6-8.5-21-13.2-31 38.3-16.6 67.8-38.4 84.7-63.2zM256 96c38.8 0 74.4 13.8 102.1 36.8-17.4 22-44.7 41.1-78.7 55.6-18.6-34.4-40-64-62.8-87.3 12.7-3.2 25.8-5.1 39.4-5.1zm-72.4 17.5c23.1 23 44.8 52.3 63.8 86.6-36.1 11-77.5 17.3-121.7 17.3-8.4 0-16.6-.3-24.7-.8 11.5-45.1 42-82.5 82.6-103.1zM96.3 248.4c9.1.4 18.3.6 27.6.5 50.4-.6 97.3-8.5 137.6-21.4 3.8 7.9 7.4 16 10.8 24.3-5.5 1.3-10.4 2.7-14.3 4.3-55.1 23.1-98.5 60.4-122 105.5-24.8-28.2-40-65.1-40-105.6 0-2.6.1-5.1.3-7.6zM256 416c-37 0-71-12.6-98.1-33.7 21.3-42.2 59.3-77.1 107.2-98.8 4.5-2.1 10.5-3.8 17.4-5.3 5.7 15.8 10.8 32.2 15.3 49.2 6.9 26.5 11.8 52.7 14.8 78.1C295 412.2 276 416 256 416zm86.5-25.5c-3-25.7-7.9-52.1-14.9-78.9-3.4-13-7.3-25.6-11.5-37.9 31.4-2.6 69-2.2 98.9 0-5.4 49.1-33 91.3-72.5 116.8z"/></svg>                            </a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/casprine">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="github"><path d="M256 32C132.3 32 32 134.9 32 261.7c0 101.5 64.2 187.5 153.2 217.9 1.4.3 2.6.4 3.8.4 8.3 0 11.5-6.1 11.5-11.4 0-5.5-.2-19.9-.3-39.1-8.4 1.9-15.9 2.7-22.6 2.7-43.1 0-52.9-33.5-52.9-33.5-10.2-26.5-24.9-33.6-24.9-33.6-19.5-13.7-.1-14.1 1.4-14.1h.1c22.5 2 34.3 23.8 34.3 23.8 11.2 19.6 26.2 25.1 39.6 25.1 10.5 0 20-3.4 25.6-6 2-14.8 7.8-24.9 14.2-30.7-49.7-5.8-102-25.5-102-113.5 0-25.1 8.7-45.6 23-61.6-2.3-5.8-10-29.2 2.2-60.8 0 0 1.6-.5 5-.5 8.1 0 26.4 3.1 56.6 24.1 17.9-5.1 37-7.6 56.1-7.7 19 .1 38.2 2.6 56.1 7.7 30.2-21 48.5-24.1 56.6-24.1 3.4 0 5 .5 5 .5 12.2 31.6 4.5 55 2.2 60.8 14.3 16.1 23 36.6 23 61.6 0 88.2-52.4 107.6-102.3 113.3 8 7.1 15.2 21.1 15.2 42.5 0 30.7-.3 55.5-.3 63 0 5.4 3.1 11.5 11.4 11.5 1.2 0 2.6-.1 4-.4C415.9 449.2 480 363.1 480 261.7 480 134.9 379.7 32 256 32z"/></svg>                            </a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/casprine_ix">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="twitter"><path d="M492 109.5c-17.4 7.7-36 12.9-55.6 15.3 20-12 35.4-31 42.6-53.6-18.7 11.1-39.4 19.2-61.5 23.5C399.8 75.8 374.6 64 346.8 64c-53.5 0-96.8 43.4-96.8 96.9 0 7.6.8 15 2.5 22.1-80.5-4-151.9-42.6-199.6-101.3-8.3 14.3-13.1 31-13.1 48.7 0 33.6 17.2 63.3 43.2 80.7-16-.4-31-4.8-44-12.1v1.2c0 47 33.4 86.1 77.7 95-8.1 2.2-16.7 3.4-25.5 3.4-6.2 0-12.3-.6-18.2-1.8 12.3 38.5 48.1 66.5 90.5 67.3-33.1 26-74.9 41.5-120.3 41.5-7.8 0-15.5-.5-23.1-1.4C62.8 432 113.7 448 168.3 448 346.6 448 444 300.3 444 172.2c0-4.2-.1-8.4-.3-12.5C462.6 146 479 129 492 109.5z"/></svg>                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
