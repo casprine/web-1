7a4e285a3e6a65c79b233e0f927e094602b85615
Casprine Assempah <casprine.001@gmail.com>
2018-07-25 08:47:04 +0000

    more work done

--- src/components/work/Content.vue
+++ src/components/work/Content.vue
@@ -2,10 +2,53 @@
 <div class="work-content">
 <div class="hero">
   <p>I work  <a href="" class="link">@OmgDigital</a> as a frontend developer  </p>
-<p>currently working with <span class="stack">React</span>, <span class="stack">Firebase</span> and <span class="stack">Sass</span>   </p>
+<p>currently working with <span class="stack">React</span>,<span class="stack">Firebase</span> and <span class="stack">Sass</span>   </p>
 </div>
 <div class="previous">
 <h1>previous works</h1>
+<a href="">
+<div class="project">
+  <div class="project-details">
+    <img src="../../assets/images/sun.png" alt="afradio's image"> 
+    <p class="name"> AFradio</p>
+  </div>
+  <div class="project-position">
+    Frontend Developer
+  </div>
+  <div class="project-description">
+   <p> Lorem ipsum dolor sit amet consectetur,
+    adipisicing elit. Quaerat tenetur cum amet non nisi temporibus
+    </p>
+  </div>
+  <div class="project-stacks">
+    <span class="stack"> React </span>
+    <span class="stack"> React </span>
+    <span class="stack"> React </span>
+    <span class="stack"> React </span>
+  </div>
+</div>
+</a>
+
+<div class="project">
+  <div class="project-details">
+    <img src="../../assets/images/sun.png" alt="afradio's image"> 
+    <p class="name"> AFradio</p>
+  </div>
+  <div class="project-position">
+    Frontend Developer
+  </div>
+  <div class="project-description">
+   <p> Lorem ipsum dolor sit amet consectetur,
+    adipisicing elit. Quaerat tenetur cum amet non nisi temporibus
+    </p>
+  </div>
+  <div class="project-stacks">
+    <span class="stack"> React </span>
+    <span class="stack"> React </span>
+    <span class="stack"> React </span>
+    <span class="stack"> React </span>
+  </div>
+</div>
 </div>
 <div class="oss">
 </div>
