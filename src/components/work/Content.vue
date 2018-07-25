fbadef753527965ee367906422bb805cc3cc12cb
Casprine Assempah <casprine.001@gmail.com>
2018-07-25 07:54:22 +0000

    minor changes

--- src/components/work/Content.vue
+++ src/components/work/Content.vue
@@ -1,11 +1,11 @@
 <template>
 <div class="work-content">
 <div class="hero">
-  <p> I work  <a href="" class="link">@OmgDigital</a> as a frontend developer  </p>
+  <p>I work  <a href="" class="link">@OmgDigital</a> as a frontend developer  </p>
 <p>currently working with <span class="stack">React</span>, <span class="stack">Firebase</span> and <span class="stack">Sass</span>   </p>
 </div>
 <div class="previous">
-
+<h1>previous works</h1>
 </div>
 <div class="oss">
 </div>
