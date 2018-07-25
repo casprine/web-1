bf6ebb28b05102a1945dabe5d1b9ce0c3ef11ba3
Casprine Assempah <casprine.001@gmail.com>
2018-07-25 10:45:48 +0000

    update

--- src/components/work/Content.vue
+++ src/components/work/Content.vue
@@ -10,10 +10,10 @@
 <div class="project">
   <div class="project-details">
     <img src="../../assets/images/sun.png" alt="afradio's image"> 
-    <p class="name"> AFradio</p>
+    <p class="name flex"> AFradio</p>
   </div>
   <div class="project-position">
-    Frontend Developer
+    <p> Frontend Developer </p>
   </div>
   <div class="project-description">
    <p> Lorem ipsum dolor sit amet consectetur,
@@ -29,26 +29,6 @@
 </div>
 </a>
 
-<div class="project">
-  <div class="project-details">
-    <img src="../../assets/images/sun.png" alt="afradio's image"> 
-    <p class="name"> AFradio</p>
-  </div>
-  <div class="project-position">
-    Frontend Developer
-  </div>
-  <div class="project-description">
-   <p> Lorem ipsum dolor sit amet consectetur,
-    adipisicing elit. Quaerat tenetur cum amet non nisi temporibus
-    </p>
-  </div>
-  <div class="project-stacks">
-    <span class="stack"> React </span>
-    <span class="stack"> React </span>
-    <span class="stack"> React </span>
-    <span class="stack"> React </span>
-  </div>
-</div>
 </div>
 <div class="oss">
 </div>
