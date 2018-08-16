57edc9ee1759fc9c5f855ebe17f01720b5d58f5f
Casprine Assempah <casprine.001@gmail.com>
2018-08-16 07:58:14 +0000

    removed api from projects

--- src/components/work/Project.vue
+++ src/components/work/Project.vue
@@ -1,39 +1,33 @@
 <template>
-<div class="projects">
+  <div class="projects">
     <div class="project">
-  <div class="project-details">
-    <img :src="project.imgLink" :alt="project.name">
-    <p class="name flex"> {{project.name}}</p>
+      <div class="project-details">
+        <img :src="project.imgLink" :alt="project.name">
+        <p class="name flex"> {{project.name}}</p>
+      </div>
+      <div class="project-position">
+        <p> {{ project.position}} </p>
+      </div>
+      <div class="project-description">
+        <p> {{ project.description }} </p>
+      </div>
+      <div class="project-stacks" id="stack">
+        <span> {{project.stacks}} </span>
+      </div>
+    </div>
   </div>
-  <div class="project-position">
-    <p> {{ project.position}} </p>
-  </div>
-  <div class="project-description">
-   <p> {{ project.description }} </p>
-  </div>
-  <div class="project-stacks" id="stack">
- <span> {{project.stacks}} </span>
-  </div>
-</div>
-</div>
 </template>
 
 <script>
-import axios from "axios";
-    export default {
-        name: "Project",
-        props: ["project" , "index"],
-        data(){
-          return{
-            project:null
-          };
-        },
-        beforeMount() {
-          let url = "https://api.myjson.com/bins/kk0de";
-          axios.get(url)
-          .then(response => (this.project = response.data))
-          .catch(error => console.log(error));
-        },
-          }
-
+  export default {
+    name: "Project",
+    props: {
+      project: Object
+    },
+    data() {
+      return {
+        return: {}
+      };
+    }
+  }
 </script>
