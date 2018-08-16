3f9cf1efefcbb10dd161c45242f695f7b3193c67
Casprine Assempah <casprine.001@gmail.com>
2018-08-16 11:31:33 +0000

    details on the project card

--- src/components/work/ProjectCard.vue
+++ src/components/work/ProjectCard.vue
@@ -4,9 +4,7 @@
         <img :src="project.imgLink" :alt="project.name">
         <p class="name flex"> {{project.name}}</p>
       </div>
-      <div class="project-position">
-        <p> {{ project.position}} </p>
-      </div>
+
       <div class="project-description">
         <p> {{ project.description }} </p>
       </div>
