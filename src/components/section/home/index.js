37ba8380985583234c8de23e43cf3d5e263974f1
Casprine <casprine.001@gmail.com>
2018-11-13 11:55:42 +0000

    update on the homepage.. new fonts , button

--- src/components/section/home/index.js
+++ src/components/section/home/index.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import { Header, Layout, Button } from "../../common/index";
+import wavingHand from "../../../assets/waving.gif";
 import "./style.scss";
 
 const HomeHeader = () => {
@@ -7,10 +8,22 @@ const HomeHeader = () => {
     <Fragment>
       <div className="ca-homeHeader">
         <Header />
-        <div className="ca-homeHeader-content padding">Hello</div>
+        <div className="ca-homeHeader-content padding">
+          <div className="ca-emoji">
+            <img src={wavingHand} alt="waving hand" />
+          </div>
+
+          <div className="ca-heading">
+            <div className="ca-heading-big">
+              Hi there , I'm Casprine, designer,
+            </div>
+            <div className="ca-heading-big">
+              frontend developer and ui enthusiast
+            </div>
+          </div>
+        </div>
         <div className="ca-homeHeader-footer padding center">
           <div className="more"> More</div>
-
           <Button content="Hire Me 👨🏿‍💻" />
         </div>
       </div>
