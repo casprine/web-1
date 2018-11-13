8eaf436e9644245874a9700193f23820e4b8c8f7
Casprine <casprine.001@gmail.com>
2018-11-13 12:31:49 +0000

    added sub heading

--- src/components/section/home/index.js
+++ src/components/section/home/index.js
@@ -15,10 +15,21 @@ const HomeHeader = () => {
 
           <div className="ca-heading">
             <div className="ca-heading-big">
-              Hi there , I'm Casprine, designer,
+              Hi there, I'm Casprine, designer,
             </div>
             <div className="ca-heading-big">
-              frontend developer and ui enthusiast
+              frontend developer and ui enthusiast.
+            </div>
+
+            <div className="ca-sub-heading">
+              <span>
+                I design and develop experiences that make people's lives simple
+              </span>
+              <br />
+              <span>
+                Let me help you grow your business & make your product look
+                pretty while I'm at it.
+              </span>
             </div>
           </div>
         </div>
