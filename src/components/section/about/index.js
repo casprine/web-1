bf19c9af0a6e4e78f096f47584545a13015d1e8f
Casprine <casprine.001@gmail.com>
2018-11-20 17:16:35 +0000

    moved all source of data into data/index.js file  successfully loaded data from new source of truth

--- src/components/section/about/index.js
+++ src/components/section/about/index.js
@@ -1,82 +1,50 @@
-import React, { Fragment } from "react";
+import React, { Fragment, Component } from "react";
 import { SectionHeader, Layout } from "../../common/index";
-import Mobile from "../../../assets/svg/mobile.svg";
-import jsSvg from "../../../assets/svg/js.svg";
-import reactSvg from "../../../assets/svg/react.svg";
-import htmlSvg from "../../../assets/svg/html.svg";
-import pwaSvg from "../../../assets/svg/pwa.png";
-import gatsbySvg from "../../../assets/svg/gatsby.png";
-import vueSvg from "../../../assets/svg/vue.png";
-import vuexSvg from "../../../assets/svg/vuex.png";
-import nextSvg from "../../../assets/svg/next.png";
-import cssSvg from "../../../assets/svg/css.png";
-import sassSvg from "../../../assets/svg/sass.png";
+import SkillSetData from "../../../data/index";
 
 import "./style.scss";
 
-const About = () => {
+const SkillSet = ({ name, description, image, skills }) => {
+  console.log(skills);
   return (
-    <Fragment>
-      <div className="about">
-        <SectionHeader />
-
-        <div className="about-skills">
-          <div className="skill">
-            <div className="skill-icon">
-              <img src={Mobile} alt="" className="img" />
-            </div>
-            <div className="skill-name">web design</div>
-            <div className="skill-description">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Obcaecati omnis, debitis consequuntur rem sapiente alias porro,
-            </div>
-
-            <div className="skill-skillsets">
-              <span className="skillset">
-                <img src={jsSvg} alt="" />
-              </span>
-              <span className="skillset">
-                <img src={reactSvg} alt="" />
-              </span>
-
-              <span className="skillset">
-                <img src={htmlSvg} alt="" />
-              </span>
-
-              <span className="skillset">
-                <img src={pwaSvg} alt="" />
-              </span>
-
-              <span className="skillset">
-                <img src={gatsbySvg} alt="" />
-              </span>
-
-              <span className="skillset">
-                <img src={vueSvg} alt="" />
-              </span>
-
-              <span className="skillset">
-                <img src={vuexSvg} alt="" />
-              </span>
-
-              <span className="skillset">
-                <img src={nextSvg} alt="" />
-              </span>
-
-              <span className="skillset">
-                <img src={cssSvg} alt="" />
-              </span>
-
-              <span className="skillset">
-                <img src={sassSvg} alt="" />
-              </span>
-            </div>
-          </div>
-        </div>
+    <div className="skill">
+      <div className="skill-icon">
+        <img src={image} alt="" className="img" />
       </div>
-    </Fragment>
+      <div className="skill-name"> {name} </div>
+      <div className="skill-description">{description} </div>
+
+      <div className="skill-skillsets">
+        {skills.map(skill => {
+          return (
+            <span className="skillset">
+              <img src={skill.imgLink} alt="" key={skill.id} />
+            </span>
+          );
+        })}
+      </div>
+    </div>
   );
 };
+
+class About extends Component {
+  componentDidMount() {}
+
+  render() {
+    return (
+      <Fragment>
+        <div className="about">
+          <SectionHeader />
+          <div className="about-skills">
+            {SkillSetData.map(data => (
+              <SkillSet {...data} key={data.id} />
+            ))}
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
+}
 
 const AboutSectionWithLayout = () => (
   <Fragment>
---
+++ src/data/index.js
@@ -0,0 +1,101 @@
+import Mobile from "../assets/svg/mobile.svg";
+import jsSvg from "../assets/svg/js.svg";
+import reactSvg from "../assets/svg/react.svg";
+import htmlSvg from "../assets/svg/html.svg";
+import pwaSvg from "../assets/svg/pwa.png";
+import gatsbySvg from "../assets/svg/gatsby.jpg";
+import vueSvg from "../assets/svg/vue.png";
+import vuexSvg from "../assets/svg/vuex.png";
+import nextSvg from "../assets/svg/next.png";
+import cssSvg from "../assets/svg/css.png";
+import sassSvg from "../assets/svg/sass.png";
+
+const skillSetData = [
+  {
+    id: "1",
+    name: "frontend developement",
+    description: ` Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati
+      omnis, debitis consequuntur rem sapiente alias porro`,
+    image: Mobile,
+    skills: [
+      {
+        imgLink: jsSvg
+      },
+      {
+        imgLink: reactSvg
+      },
+      {
+        imgLink: htmlSvg
+      },
+      {
+        imgLink: pwaSvg
+      },
+      {
+        imgLink: gatsbySvg
+      },
+      {
+        imgLink: vueSvg
+      },
+      {
+        imgLink: vuexSvg
+      },
+      {
+        imgLink: nextSvg
+      },
+      {
+        imgLink: nextSvg
+      },
+      {
+        imgLink: cssSvg
+      },
+      {
+        imgLink: sassSvg
+      }
+    ]
+  },
+
+  {
+    id: "2",
+    name: "frontend developement",
+    description: ` Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati
+      omnis, debitis consequuntur rem sapiente alias porro`,
+    image: Mobile,
+    skills: [
+      {
+        jsSvg,
+        reactSvg,
+        htmlSvg,
+        pwaSvg,
+        gatsbySvg,
+        vueSvg,
+        vuexSvg,
+        nextSvg,
+        cssSvg,
+        sassSvg
+      }
+    ]
+  },
+  {
+    id: "3",
+    name: "frontend developement",
+    description: ` Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati
+      omnis, debitis consequuntur rem sapiente alias porro`,
+    image: Mobile,
+    skills: [
+      {
+        jsSvg,
+        reactSvg,
+        htmlSvg,
+        pwaSvg,
+        gatsbySvg,
+        vueSvg,
+        vuexSvg,
+        nextSvg,
+        cssSvg,
+        sassSvg
+      }
+    ]
+  }
+];
+
+export default skillSetData;
