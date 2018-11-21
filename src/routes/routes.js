a423121db14796f4339cb44ccc32439950fa1f1c
Casprine <casprine.001@gmail.com>
2018-11-21 13:16:25 +0000

    minor update

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,8 @@
 import React, { Fragment, lazy, Suspense } from "react";
 import { Switch, Route } from "react-router-dom";
 
-const Home = lazy(() => import("../components/pages/home"));
+// const Home = lazy(() => import("../components/pages/home"));
+import Home from  "../components/pages/home"
 const Design = lazy(() => import("../components/pages/design"));
 const Work = lazy(() => import("../components/pages/work"));
 
@@ -9,10 +10,11 @@ const Main = () => (
   <Fragment>
     <Switch>
       <Route exact path="/" component={Home} />
+
       <Suspense fallback={<div>Loading...</div>}>
         <Route exact path="/work" component={Work} />
-        <Route exact path="/work" component={Design} />
       </Suspense>
+      <Route exact path="/work" component={Design} />
     </Switch>
   </Fragment>
 );
