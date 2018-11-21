fae6d3e3928d7d39553dc5ce9471ff2c9119f6b3
Casprine <casprine.001@gmail.com>
2018-11-21 11:31:40 +0000

    added more content on footer

--- src/components/section/footer/index.js
+++ src/components/section/footer/index.js
@@ -6,6 +6,28 @@ const Footer = () => (
   <Fragment>
     <div className="footer">
       <Logo />
+
+      <div className="text">
+        Have a nice day
+        <span role="img" aria-label="smile">
+          😁
+        </span>
+      </div>
+
+      <div className="links">
+        <a
+          href="https://dribbble.com/casprine"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link"
+        >
+          {" "}
+          Dribbble{" "}
+        </a>
+        <a href="https://www.uplabs.com/casprine"> UpLabs </a>
+        <a href="https://twitter.com/casprine_ix"> Twitter</a>
+        <a href="https://twitter.com/casprine_ix"> Github</a>
+      </div>
     </div>
   </Fragment>
 );
